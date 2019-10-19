7106167d979a7720848298ae36bf5475f9f0a4d3
Dav991123 <sargsyand89@gmail.com>
2019-10-19 19:16:58 +0400

    add useState hook dnoe end important

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -2,34 +2,37 @@ import React, {useState} from 'react';
 
 import './todo-list-item.css';
 
-const TodoListItem = ({ label, important = false }) => {
+const TodoListItem = ({ label }) => {
   const [done, setDone] = useState(false);
-
+  const [important, setImportant] = useState(false)
   const style = {
     color: important ? 'steelblue' : 'black',
     fontWeight: important ? 'bold' : 'normal'
   };
-  const onLabelClick = () => {
-      console.log(label)
-  }
 
   return (
-    <span className="todo-list-item">
+    <span
+        className={`todo-list-item ${done ? 'done': ''}`}
+
+    >
       <span
         className="todo-list-item-label"
-        style={style}
-        onClick={onLabelClick}
+        style={important ? style : {}}
+        onClick={() => setDone(!done)}
       >
         {label}
       </span>
 
       <button type="button"
-              className="btn btn-outline-success btn-sm float-right">
+              className="btn btn-outline-success btn-sm float-right"
+              onClick={() => setImportant(!important)}
+      >
         <i className="fa fa-exclamation" />
       </button>
 
       <button type="button"
-              className="btn btn-outline-danger btn-sm float-right">
+              className="btn btn-outline-danger btn-sm float-right"
+      >
         <i className="fa fa-trash-o" />
       </button>
     </span>
